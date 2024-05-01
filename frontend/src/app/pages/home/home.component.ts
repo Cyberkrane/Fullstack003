24e29615aab4a66465dc4c62a8c853d88025d461
Rolando Corral <cyberkrane@gmail.com>
2024-05-01 17:50:15 -0300

    layout header finished

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +6,24 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+
+  @ViewChild('full_image_box') full_image_box: any;
+  @ViewChild('full_image') full_image: any;
   
   title: string = 'Home';
+
+
+  constructor() { }
+
+
+  openImg(img: string) {
+    this.full_image_box.nativeElement.style.display = 'flex';
+    this.full_image.nativeElement.src = img;
+    console.log('open');
+  }
+  closeImg() {
+    this.full_image_box.nativeElement.style.display = 'none';
+    console.log('close');
+  }
 
 }
