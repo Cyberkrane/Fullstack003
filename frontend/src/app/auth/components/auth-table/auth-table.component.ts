787a50e4ea9c542e4b0fd0d714ea303953ec77e5
Rolando Corral <cyberkrane@gmail.com>
2024-05-07 14:20:56 -0300

    completando estilos el auth

--- frontend/src/app/auth/components/auth-table/auth-table.component.ts
+++ frontend/src/app/auth/components/auth-table/auth-table.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../interfaces/user';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-auth-table',
   templateUrl: './auth-table.component.html',
   styleUrls: ['./auth-table.component.scss']
 })
-export class AuthTableComponent {
+export class AuthTableComponent implements OnInit {
+
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe(
+      (users) => { this.users = users; }
+    )
+  }
+
+  editUser(_t13: User) {
+   console.log('Editando.....',_t13);
+  }
+  
+  deleteUser(_t13: User) {
+    console.log('Borrando.....',_t13);
+  }
 
 }
